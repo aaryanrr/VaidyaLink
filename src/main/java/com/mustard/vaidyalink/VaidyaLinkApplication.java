5bccbc08081e8f748ea839bc44e84dd12b4d7322
aaryanrr <raiaryan2422@gmail.com>
2024-10-26 00:44:26 +0530

    load env file

--- src/main/java/com/mustard/vaidyalink/VaidyaLinkApplication.java
+++ src/main/java/com/mustard/vaidyalink/VaidyaLinkApplication.java
@@ -2,11 +2,14 @@ package com.mustard.vaidyalink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
 public class VaidyaLinkApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
         SpringApplication.run(VaidyaLinkApplication.class, args);
     }
 }
