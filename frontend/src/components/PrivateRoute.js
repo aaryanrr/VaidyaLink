a4562022975177fa18e734f83b2f25715012be3a
aaryanrr <raiaryan2422@gmail.com>
2024-10-26 07:37:00 +0530

    add user private route

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import {Navigate} from 'react-router-dom';
+import {Navigate, useLocation} from 'react-router-dom';
 
 const PrivateRoute = ({children}) => {
     const token = localStorage.getItem('token');
+    const location = useLocation();
 
-    return token ? children : <Navigate to="/institution" replace/>;
+    if (!token) {
+        if (location.pathname.includes('institution-dashboard') || location.pathname.includes('invite-new-user')) {
+            return <Navigate to="/institution" replace/>;
+        }
+        return <Navigate to="/users" replace/>;
+    }
+
+    return children;
 };
 
 export default PrivateRoute;
