38d786e56453e1c33180e2acc3e1eeb4401d6787
aaryanrr <raiaryan2422@gmail.com>
2024-11-24 13:56:46 +0530

    about us page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import './css/About.css';
+import logo from '../assets/Logo.png';
+
 function About() {
     return (
-        <div>
-            <h1>About VaidyaLink</h1>
-            <p>Information about the project and team.</p>
+        <div className="about-container">
+            <main className="about-main">
+                <header className="about-header">
+                    <img src={logo} className="about-logo" alt="VaidyaLink Logo"/>
+                </header>
+                <h1>VaidyaLink - About Us</h1>
+                <p>VaidyaLink is a platform for Secure and Efficient storage of medical data.<br/>
+                    Leveraging the Blockchain technology, the platform provides a decentralized<br/>
+                    way to store patient data and provides the patient with granular control over their data.</p>
+            </main>
         </div>
     );
 }
