5e223415395a1491ec2a65ff67a6f8fbd99397c2
aaryanrr <raiaryan2422@gmail.com>
2025-04-28 00:14:13 +0530

    Create new password util to generate secure random passwords

---
+++ src/main/java/com/mustard/vaidyalink/utils/PasswordUtil.java
@@ -0,0 +1,40 @@
+package com.mustard.vaidyalink.utils;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PasswordUtil {
+
+    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL = "!@#$%^&*-_+=";
+    private static final int PASSWORD_LENGTH = 12;
+
+    public static String generateSecurePassword() {
+        SecureRandom random = new SecureRandom();
+        List<Character> passwordChars = new ArrayList<>();
+
+        for (int i = 0; i < 2; i++) {
+            passwordChars.add(SPECIAL.charAt(random.nextInt(SPECIAL.length())));
+        }
+        for (int i = 0; i < 2; i++) {
+            passwordChars.add(DIGITS.charAt(random.nextInt(DIGITS.length())));
+        }
+        passwordChars.add(UPPER.charAt(random.nextInt(UPPER.length())));
+
+        while (passwordChars.size() < PASSWORD_LENGTH) {
+            passwordChars.add(LOWER.charAt(random.nextInt(LOWER.length())));
+        }
+
+        Collections.shuffle(passwordChars, random);
+
+        StringBuilder password = new StringBuilder();
+        for (char c : passwordChars) {
+            password.append(c);
+        }
+        return password.toString();
+    }
+}
